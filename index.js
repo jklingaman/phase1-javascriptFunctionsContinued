0c662de496942f4a6fbf75cbb7554dd02b1e2cd6
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-02-07 11:31:08 -0600

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(special = '*'){
+   const greeting = "You are";
+    return function (defined = 'a hard worker') {
+        return `${greeting} ${special}${defined}${special}!`;
+        }
+    }
